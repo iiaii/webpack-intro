31869d473adb8f5c21bcf3b366e7e9f3c99f55c9
iiaii <jhun9409@naver.com>
2020-08-12 23:12:55 +0900

    주석 추가

--- getting-started/webpack.config.js
+++ getting-started/webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
+    entry: './app/index.js',        // 배열, 객체 형태도 가능
+    output: {                       // 출력 위치
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
